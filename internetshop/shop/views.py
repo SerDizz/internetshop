17ae4b57b01e65a8a163ff017ba500e0e94a11b6
SerDizz <cenibrat@mail.ru>
2024-07-11 12:41:09 +0500

    Починил строку поиска

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -22,7 +22,8 @@ def home(request):
 
 def view_product(request, id):
     product = Product.objects.filter(id=id).first()
-    print(product)
+    review = product.review_set.all()
     return render(request, "product.html", {
-        'product': product
+        'product': product,
+        'review': review,
     })
